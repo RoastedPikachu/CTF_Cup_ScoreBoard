5ccd5dba3c859b64c8f16b3edf088e710463dd9d
Boris Karabut <boriskarabytwork@gmail.com>
2023-12-09 10:14:48 +0300

    Поменял расширение файлов

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -3,7 +3,7 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-import CompetitorReducer from "../store/storeReducers/Competitor";
+import CompetitorReducer from "./storeReducers/Competitor";
 
 const persistConfig = {
   key: "root",
